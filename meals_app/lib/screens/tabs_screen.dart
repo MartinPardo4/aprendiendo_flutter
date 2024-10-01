b4801f473517f397c7f9a7163c7ddfd1a4ca3228
Martin Pardo <martupardo@hotmail.com>
2024-10-01 17:52:25 -0300

    Drawer added

--- meals_app/lib/screens/tabs_screen.dart
+++ meals_app/lib/screens/tabs_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:meals_app/models/meal.dart';
 import 'package:meals_app/screens/categories.dart';
 import 'package:meals_app/screens/meals_screen.dart';
+import 'package:meals_app/widgets/main_drawer.dart';
 
 class TabsScreen extends StatefulWidget{
 
@@ -49,6 +50,7 @@ class _TabsScreenState extends State<TabsScreen>{
       appBar: AppBar(
         title: Text(currentPageTitle),
       ),
+      drawer: const MainDrawer(),
       body: currentPage,
       bottomNavigationBar: BottomNavigationBar(
         onTap: _selectPage,
---
+++ meals_app/lib/widgets/main_drawer.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+class MainDrawer extends StatelessWidget {
+  const MainDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: [
+          DrawerHeader(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Theme.of(context).colorScheme.primaryContainer,
+                  Theme.of(context)
+                      .colorScheme
+                      .primaryContainer
+                      .withOpacity(0.7),
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+            ),
+            child: Row(
+              children: [
+                Icon(
+                  Icons.fastfood,
+                  size: 48,
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+                const SizedBox(width: 16),
+                Text(
+                  "Cooking Up!",
+                  style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                        color: Theme.of(context).colorScheme.primary,
+                      ),
+                ),
+              ],
+            ),
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.restaurant,
+              size: 24,
+              color: Theme.of(context).colorScheme.onSurface,
+            ),
+            title: Text(
+              "Meals",
+              style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                    color: Theme.of(context).colorScheme.primary,
+                    fontSize: 24
+                  ),
+                  
+            ),
+            //subtitle: ,
+            onTap: () {},
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.settings,
+              size: 24,
+              color: Theme.of(context).colorScheme.onSurface,
+            ),
+            title: Text(
+              "Filters",
+              style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                    color: Theme.of(context).colorScheme.primary,
+                    fontSize: 24
+                  ),
+                  
+            ),
+            //subtitle: ,
+            onTap: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
