c90305c8d1374ce0b6b0c3dd09527276627d76ec
Martin Pardo <martupardo@hotmail.com>
2024-10-09 18:02:20 -0300

    chat_app added

---
+++ chat_app/lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:chat_app/screens/authentication.dart';
+import 'package:chat_app/screens/chat.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "FltterChat",
+      theme: ThemeData().copyWith(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(255, 63, 17, 177),
+        ),
+      ),
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (ctx, snapshot) {
+          if (snapshot.hasData) {
+            return const ChatScreen();
+          }
+          return const AuthenticationScreen();
+        },
+      ),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
---
+++ chat_app/lib/screens/authentication.dart
@@ -0,0 +1,157 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+final _firebase = FirebaseAuth.instance;
+
+class AuthenticationScreen extends StatefulWidget {
+  const AuthenticationScreen({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _AuthenticationScreenState();
+}
+
+class _AuthenticationScreenState extends State<AuthenticationScreen> {
+  var _isLogIn = true;
+  final _formKey = GlobalKey<FormState>();
+
+  var _enteredEmail = "";
+  var _enteredPassword = "";
+
+  void _submit() async {
+    var isValid = _formKey.currentState!.validate();
+
+    if (!isValid) {
+      return;
+    }
+
+    _formKey.currentState!.save();
+
+    try {
+      if (_isLogIn) {
+        final userCredentials = await _firebase.signInWithEmailAndPassword(
+            email: _enteredEmail,
+            password: _enteredPassword,
+          );
+          print(userCredentials);
+      } else {
+        
+        final userCredentials = await _firebase.createUserWithEmailAndPassword(
+          email: _enteredEmail,
+          password: _enteredPassword,
+        );
+        print(userCredentials);
+      }
+    } on FirebaseAuthException catch (error) { 
+      ScaffoldMessenger.of(context).clearSnackBars();
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(error.message ?? "Authentication failed"),
+        ),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.primary,
+      appBar: AppBar(
+        title: Text(_isLogIn ? "Log in" : "Sign Up"),
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                margin: const EdgeInsets.all(30),
+                width: 200,
+                child: Image.asset('assets/images/chat.png'),
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              Card(
+                margin: const EdgeInsets.all(30),
+                child: SingleChildScrollView(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 24, vertical: 12),
+                    child: Form(
+                      key: _formKey,
+                      child: Column(
+                        children: [
+                          TextFormField(
+                              keyboardType: TextInputType.emailAddress,
+                              autocorrect: false,
+                              textCapitalization: TextCapitalization.none,
+                              decoration: const InputDecoration(
+                                labelText: "Email adress",
+                              ),
+                              validator: (value) {
+                                if (value == null ||
+                                    value.trim().isEmpty ||
+                                    !value.contains("@")) {
+                                  return "Must be a valid email";
+                                }
+                                return null;
+                              },
+                              onSaved: (value) {
+                                setState(() {
+                                  _enteredEmail = value!;
+                                });
+                              }),
+                          const SizedBox(
+                            height: 6,
+                          ),
+                          TextFormField(
+                            keyboardType: TextInputType.emailAddress,
+                            obscureText: true,
+                            decoration: const InputDecoration(
+                              labelText: "Password",
+                            ),
+                            validator: (value) {
+                              if (value == null || value.trim().length < 6) {
+                                return "Must be a valid password";
+                              }
+                              return null;
+                            },
+                            onSaved: (value) {
+                              setState(() {
+                                _enteredPassword = value!;
+                              });
+                            },
+                          ),
+                          const SizedBox(height: 14),
+                          ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Theme.of(context)
+                                    .colorScheme
+                                    .primaryContainer,
+                              ),
+                              onPressed: _submit,
+                              child: Text(_isLogIn ? "Log In" : "Sign Up")),
+                          const SizedBox(height: 6),
+                          TextButton(
+                            onPressed: () {
+                              setState(() {
+                                _isLogIn = !_isLogIn;
+                              });
+                            },
+                            child: Text(_isLogIn
+                                ? "Create an account"
+                                : "I have an account"),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
